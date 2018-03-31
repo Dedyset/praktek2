87b936a27e7263c3d813f58447c2e70d32901145
Dedyset <dedy.smkn2bjb@gmail.com>
2018-03-31 09:37:30 +0800

    mengisi nilai atribut

--- src/praktek2/helmaksi.java
+++ src/praktek2/helmaksi.java
@@ -3,6 +3,9 @@ package praktek2;
 public class helmaksi {
     public static void main(String[] args) {
         helm r1 = new helm();
+        
+        r1.harga_helm = 220000;
+        r1.jumlah_helm = 5;
     }
  
 }
