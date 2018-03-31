20696416e045d80d51811f794646aab300462aba
Dedyset <dedy.smkn2bjb@gmail.com>
2018-03-31 09:30:06 +0800

    Membuat atribut dan behavior

--- src/praktek2/helm.java
+++ src/praktek2/helm.java
@@ -1,5 +1,13 @@
 package praktek2;
 
 public class helm {
+    int harga_helm;
+    int jumlah_helm;   
     
+    void cetakInfo(){
+        System.out.println("=============================");
+        System.out.println("harga_helm      :"+harga_helm);
+        System.out.println("jumlah_helm     :"+jumlah_helm);
+        System.out.println("=============================");
+        }
 }
